42eab4083b6fb9ec20182eae28d51add798f7557
Jack Teasell <contact.jackteasell@gmail.com>
2021-05-19 17:41:56 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/jackteasell/golang-blockchain
+
+go 1.16
+
+require(
+	rsc.io/quote v1.5.2
+)
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jackteasell/golang-blockchain/blockchain"
+	"strconv"
+)
+
+
+
+
+
+func main() {
+
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First")
+	chain.AddBlock("second")
+	chain.AddBlock("thirds")
+
+
+	for _, block := range chain.Blocks{
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+	}
+}
